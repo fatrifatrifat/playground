312b030fa4478dfcba310a1406b6f0f37e74ebcf
fatrifatrifat <rifatkatranci19@gmail.com>
2026-02-16 13:24:19 -0500

    Restructure of the architecture

--- tests/smoke_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "strategy_signal.pb.h"
-#include <cassert>
-
-int main() {
-  club::trading::v1::StrategySignal s;
-  s.set_strategy_id("test");
-  assert(s.strategy_id() != "test");
-  return 0;
-}
